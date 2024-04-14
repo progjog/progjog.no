a01b85ab1b1cb36ce5b5b57d45421db0757d6bed
Kent Martin Åsvang <github.com@asvang.no>
2024-04-14 20:36:39 +0200

    added delete-method on sessions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from db import get_sessions
-from flask import Flask, render_template
+from db import delete_session, get_sessions, add_session
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,29 @@ def index():
     return render_template("index.html", context=context)
 
 
+@app.route("/session/new", methods=["POST"])
+def session_new():
+    # TODO: add flash messages
+    # TODO: add auth
+    if request.method == "POST":
+        form = request.form
+
+        title = form["title"]
+        date = form["date"]
+        details = form["details"]
+
+        add_session(title, details, date)
+
+    return redirect(url_for("index"))
+
+
+# DELETE /training-session
+@app.route("/session/delete/<id>", methods=["GET"])
+def session_delete(id):
+    delete_session(id)
+    return redirect(url_for("index"))
+
+
 """
 *GET /training-session (dont need?)
 GET /training-session/<id>
@@ -17,5 +40,4 @@ GET /training-session/<id>
 POST /training-session/ (Create)
 PUT /training-session/<id> (Update)
 
-DELETE /training-session
 """
--- db.py
+++ db.py
@@ -1,18 +1,37 @@
 import sqlite3
+import settings
+
 
 def get_sessions():
-    cur = sqlite3.connect("dev.db")
+    cur = sqlite3.connect(settings.DATABASE)
     res = cur.execute("SELECT * FROM session")
     sessions = res.fetchall()
+    cur.close()
     return sessions
 
 
-def setup():
+def add_session(title, details, date):
+    cur = sqlite3.connect(settings.DATABASE)
+    cur.execute("INSERT INTO session (title, details, date) VALUES (?, ?, ?)", (title, details, date))
+    cur.commit()
+    cur.close()
+
+
+def delete_session(id):
+    cur = sqlite3.connect(settings.DATABASE)
+    cur.execute("DELETE FROM session WHERE session.id = ?", id)
+    cur.commit()
+    cur.close()
+
+
+def setup() -> None:
     import sqlite3 
-    with open("dev.sql", "r") as sql_file:
+    with open(settings.DATABASE_SCHEMA, "r") as sql_file:
         sql_script = sql_file.read()
-        cur = sqlite3.connect("dev.db")
+        cur = sqlite3.connect(settings.DATABASE)
         cur.executescript(sql_script)
+        cur.close()
+
 
 if __name__ == "__main__":
     setup()
