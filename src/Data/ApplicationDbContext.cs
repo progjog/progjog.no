6d4fc1c8eebdb9b69e9ecb8fb82e455bd0a288b6
Kent Martin Åsvang <github.com@asvang.no>
2024-05-26 18:43:48 +0200

    delete trainingsession works

---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using progjog.Data.Entities;
+
+namespace progjog.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TrainingSession> TrainingSessions => Set<TrainingSession>();
+}
